bfd8a7c4e65a7cba2fe729a2dba2e06bf1355d1e
william <factzure@hotmail.com>
2021-09-10 11:22:15 -0500

    Agregar archivos de proyecto.

---
+++ projecto14_NumeroPares/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projecto14_NumeroPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           int x = 1;
+            int suma = 0;
+            //2 ,4 , 6  ,  8= 20
+            while (x<=10)
+            {
+                Console.WriteLine(x*2);
+                suma = suma + x * 2;
+                x = x + 1;
+            }
+
+            Console.WriteLine("la suma es  : {0} ", suma);
+            Console.Read();
+        }
+    }
+}
